90ad669194458ecffcbb1e25c9c0175956140ccf
Taen Ahammed <taenahammed7@gmail.com>
2019-11-17 12:12:18 +0600

    implemented adminlte3

---
+++ resources/views/admin/includes/scripts.blade.php
@@ -0,0 +1,11 @@
+<!-- jQuery -->
+<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE -->
+<script src="{{ asset('adminlte/js/adminlte.js') }}"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{ asset('adminlte/plugins/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('adminlte/js/demo.js') }}"></script>
+<script src="{{ asset('adminlte/js/pages/dashboard3.js') }}"></script>
